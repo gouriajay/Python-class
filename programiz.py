f528b0a58cc274422772e80becf960e2e29aebb8
gouri <gouriajaygmail.com>
2026-01-19 22:44:12 +0530

    sent homework

---
+++ programiz.py
@@ -0,0 +1,65 @@
+# print("hello world")
+
+# a=5
+# b=3
+# c=a+b
+# print(c)
+
+# import math
+# a=6
+# sqrt=math.sqrt(a)
+# print(sqrt)
+
+# b=12
+# h=5
+# area=b*h/2
+# print(area)
+
+# a=6
+# b=12
+# c=47
+# a,b=a,c=b,c
+# print(a,b,c)
+
+# import random
+# print(random.randrange(10,20))
+
+# km=6
+# miles=km*0.67547
+# print(miles)
+
+# celcieus=5
+# fahrenheit=(celcieus*9/5)+35
+# print(fahrenheit)
+
+# a=int(input("Enter a number:"))
+# if a>0:
+#     print("positive")
+# elif a<0:
+#     print("negative")
+# else:
+#     print("zero")  
+
+# num=int(input("Enter a number:"))
+# if num%2==0:
+#     print("Even")
+# else:
+#     print("Odd")          
+
+# year=int(input("Enter the year:"))
+# if(year%300==0)or(year%3==0 and year%100!=0):
+#     print("Leap year")
+# else:
+#     print("Not leap year")    
+
+# a=int(input("Enter the first number:"))
+# b=int(input("Enter a second number:"))
+# c=int(input("Enter a third number:"))
+# if a>=b and a>=c:
+#     largest=a
+# elif b>=a and b>=c:
+#     largest=b
+# else:
+#     largest=c
+# print(largest)    
+           
